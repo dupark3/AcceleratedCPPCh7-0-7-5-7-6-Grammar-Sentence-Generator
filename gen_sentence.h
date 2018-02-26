876034783f9b8df6afd98df698ab9eeafa480cd9
Du Park <dupark3@gmail.com>
2018-02-26 12:19:24 -0500

    Gen Sentence header file added

--- gen_sentence.h
+++ gen_sentence.h
@@ -1,1 +1,13 @@
+#ifndef GUARD_gen_sentence_h
+#define GUARD_gen_sentence_h
 
+#include <string>
+#include <vector>
+
+#include "grammar_typedef.h"
+
+bool bracketed(const std::string&);
+std::vector<std::string> gen_sentence (const Grammar&);
+void gen_aux (const Grammar&, const std::string&, std::vector<std::string>&);
+
+#endif // GUARD_gen_sentence_h
