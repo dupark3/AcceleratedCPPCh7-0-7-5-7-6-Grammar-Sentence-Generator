b4783bfb8c0f4a465467758ff302a1e73fac2435
Du Park <dupark3@gmail.com>
2018-02-26 12:42:11 -0500

    Added include statements

--- grammar_typedef.h
+++ grammar_typedef.h
@@ -1,6 +1,10 @@
 #ifndef GUARD_grammar_typedef_h
 #define GUARD_grammar_typedef_h
 
+#include <map>
+#include <string>
+#include <vector>
+
 typedef std::vector<std::string> Rule;
 typedef std::vector<Rule> Rule_collection;
 typedef std::map<std::string, Rule_collection> Grammar;
