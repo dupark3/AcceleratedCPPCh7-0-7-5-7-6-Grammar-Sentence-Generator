15d55ad58aee82a8e2d4e3d55f4d325b6306729d
Du Park <dupark3@gmail.com>
2018-02-27 07:31:23 -0500

    Using two vectors with insert and erase, despite being inefficient, satisfies the use of vector requirement

--- gen_sentence.cpp
+++ gen_sentence.cpp
@@ -33,22 +33,23 @@ void gen_aux (const Grammar& g, const std::string& word, std::vector<std::string
         throw std::logic_error ("empty rule"); // g did not contain <sentence>
     const Rule_collection& c = it->second; // c is the vector of vectors possible for <sentence> (only one in this case)
     const Rule& r = c[nrand(c.size())]; // r selected a random rule of <sentence>, so now holds "the", <noun-phrase>, <verb>, <location>
+    for (Rule::const_iterator i = r.begin(); i != r.end(); ++i)
+        ret.push_back(*i);
+    // ret now holds "the <noun-phrase> <verb> <location>"
 
-    for (Rule::const_iterator i = r.begin(); i != r.end(); ++i){
-        if (!bracketed(*i)){
-            ret.push_back(*i);
+    std::vector<std::string>::iterator i = ret.begin();
+    unsigned index = 0;
+    while (i != ret.end()){
+        if (!bracketed(*i)) {
+            ++i;
+            ++index;
         } else {
-            const Rule_collection& c2 = g.find(*i)->second; // c2 holds the vector of vectors if bracketed OR holds vector of strings
-            const Rule& r2 = c2[nrand(c2.size())]; // r2 holds the randomly selected vector from the vector of vectors
-            Rule::const_iterator j = r2.begin(); // j points to the beginning of the randomly selected vector of strings
-            while (bracketed(*j)){
-//                if (g.end() == g.find(*j))
-//                    throw std::logic_error("empty rule");
-                const Rule_collection& c3 = g.find(*j)->second;
-                const Rule& r3 = c3[nrand(c3.size())];
-                j = r3.begin();
-            }
-            ret.push_back(*j);
+            // randomly select from rule collection
+            const Rule_collection& c2 = g.find(*i)->second; // c2 holds the vector of vectors
+            const Rule& r2 = c2[nrand(c2.size())]; // r2 holds randomly selected vector of vectors
+            ret.insert(ret.erase(i), r2.begin(), r2.end()); // erase bracketed element and replace with new vector of strings.
+                                                            // set i to newly inserted position.
+            i = ret.begin() + index;
         }
     }
 }
